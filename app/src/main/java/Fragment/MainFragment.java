5c9d86765085ebaeeddb24a9276b372cf0fccf47
Khaled Saifullah <khaled.saif06@gmail.com>
2018-12-23 12:45:11 +0600

    Phone Authentication Fragment added and Attached with main fragment

--- app/src/main/java/Fragment/MainFragment.java
+++ app/src/main/java/Fragment/MainFragment.java
@@ -1,5 +1,6 @@
 package Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,15 +8,42 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.saif.firebaseauthenticationandrtdb.R;
 
-public class MainFragment extends Fragment {
-    View view;
+import Utils.FragmentUtilities;
+
+public class MainFragment extends Fragment implements View.OnClickListener {
+    private View view;
+    private Button btnOTPAuthentication;
+    private Context context;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        context=getActivity();
         view=inflater.inflate(R.layout.fragment_main,container,false);
+        initializeViewsandAttachListeners(view);
         return view;
+    }
+
+    private void initializeViewsandAttachListeners(View view) {
+        btnOTPAuthentication=view.findViewById(R.id.btnOTPAuthentication);
+        btnOTPAuthentication.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.btnOTPAuthentication:
+                PhoneAuthFragment phoneAuthFragment=new PhoneAuthFragment();
+                new FragmentUtilities(getActivity()).replaceFragment(R.id.container,phoneAuthFragment);
+                break;
+        }
+
     }
 }
