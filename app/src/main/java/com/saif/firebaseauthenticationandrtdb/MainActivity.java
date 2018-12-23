ff786ac904bda99985a30f0d2300d85c492d1240
Khaled Saifullah <khaled.saif06@gmail.com>
2018-12-23 12:33:59 +0600

    Initial Fragment added

--- app/src/main/java/com/saif/firebaseauthenticationandrtdb/MainActivity.java
+++ app/src/main/java/com/saif/firebaseauthenticationandrtdb/MainActivity.java
@@ -3,11 +3,17 @@ package com.saif.firebaseauthenticationandrtdb;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import Fragment.MainFragment;
+import Utils.FragmentUtilities;
+
 public class MainActivity extends AppCompatActivity {
+    private  MainFragment mainFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainFragment=new MainFragment();
+        new FragmentUtilities(this).addFragment(R.id.container,mainFragment);
     }
 }
